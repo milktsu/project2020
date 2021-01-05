787aff8a23e0f966b3260e2f211d19b3cfdfb345
Angelina <fennyc0310@gmail.com>
2021-01-05 14:48:21 +0800

    initial commit

--- src/com/fju/SwitchTester.java
+++ src/com/fju/SwitchTester.java
@@ -4,7 +4,11 @@ public class SwitchTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int n =1;
+		switch(n) {
+		case 1:
+			System.out.println("1");
+		}
 	}
 
 }
